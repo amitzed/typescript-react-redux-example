a9b47f564078b9e281dc2b3f8e1680b098cea35d
amitzed <amitzmn@gmail.com>
2021-01-05 15:10:02 -0500

    Added counter to App class

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends Component {
+interface AppProps {
+  color?: string; // Question mark in key indicates that the color prop in <App /> is optional and not required;
+}
+
+class App extends Component<AppProps> {
+  state = {
+    counter: 0
+  }
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 })
+  }
+
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1})
+  }
+
   render() {
     return (
       <div>
-        App
+        {/* <h1>{this.props.color}</h1> */}
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        <h1>{this.state.counter}</h1>
       </div>
     )
   }
 }
 
 ReactDOM.render(
-  <App />,
+  <App color="red" />,
   document.getElementById('root')
 )
