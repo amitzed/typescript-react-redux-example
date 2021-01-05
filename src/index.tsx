257cf12c63a148b695550e34fc1095a7b817ba30
amitzed <amitzmn@gmail.com>
2021-01-05 15:43:42 -0500

    Setup basic Redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,21 @@
 import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
 
-interface AppProps {
-  color?: string; // Question mark in key indicates that the color prop in <App /> is optional and not required;
-}
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-class App extends Component<AppProps> {
-  state = {
-    counter: 0
-  }
+import { reducers } from './reducers';
+import { App } from './components/App';
 
-  onIncrement = (): void => {
-    this.setState({ counter: this.state.counter + 1 })
-  }
+const store = createStore(reducers, applyMiddleware(thunk));
 
-  onDecrement = (): void => {
-    this.setState({ counter: this.state.counter - 1})
-  }
 
-  render() {
-    return (
-      <div>
-        <h2>Class-Based Component</h2>
-        {/* <h1>{this.props.color}</h1> */}
-        <button onClick={this.onIncrement}>Increment</button>
-        <button onClick={this.onDecrement}>Decrement</button>
-        <h1>{this.state.counter}</h1>
-      </div>
-    )
-  }
-};
 
 
 ReactDOM.render(
-  <App color="red" />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 )
