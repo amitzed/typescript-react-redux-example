44a2786855ea13064192b80ff55f3610ca393938
amitzed <amitzmn@gmail.com>
2021-01-05 19:42:59 -0500

    List from jsonPlaceholder API displaying in browser

---
+++ src/components/App.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { Todo, fetchTodos }  from '../actions';
+import { StoreState } from '../reducers';
+
+interface AppProps {
+  todos: Todo[];
+  fetchTodos(): any;
+}
+
+class _App extends React.Component<AppProps> {
+  onButtonClick = (): void => {
+    this.props.fetchTodos();
+  };
+
+  renderList(): JSX.Element[] {
+    return this.props.todos.map((todo: Todo) => {
+      return (
+        <div key={todo.id}>
+          {todo.title}
+        </div>
+      )
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.onButtonClick}>Fetch</button>
+        {this.renderList()}
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = ({todos}: StoreState): { todos: Todo[]} => {
+  return { todos }
+};
+
+export const App = connect(
+  mapStateToProps,
+  { fetchTodos }
+)(_App);
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,5 +1,11 @@
 import { combineReducers } from 'redux';
+import { todosReducer } from './todos';
+import { Todo } from '../actions';
 
-export const reducers = combineReducers({
-  replaceMe: () => 123
+export interface StoreState {
+  todos: Todo[]
+}
+
+export const reducers = combineReducers<StoreState>({
+  todos: todosReducer
 });
